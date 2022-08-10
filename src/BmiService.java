40f82e1769dc402f423457d377d57db95cb0f7a2
SBirdperson <s.birdperson@gmail.com>
2022-08-10 15:54:19 +0300

    ReformatCode

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public double calculate(double mass, double height){
-          double IMT = (mass *10000) /(height*height);
-        return  IMT;
+    public double calculate(double mass, double height) {
+        double IMT = (mass * 10000) / (height * height);
+        return IMT;
     }
 }
--- src/bodyMassIndex.java
+++ src/bodyMassIndex.java
@@ -1,15 +1,16 @@
 public class bodyMassIndex {
     public static void main(String[] args) {
 
-BmiService bmiService =new BmiService();
-     double a =   bmiService.calculate(70.0,169.0);
+        BmiService bmiService = new BmiService();
+        double a = bmiService.calculate(70.0, 169.0);
 
-        System.out.println("-ВАШ ИНДЕКС МАССЫ ТЕЛА (ИМТ): "+ a + "\n" +
+        System.out.println("-ВАШ ИНДЕКС МАССЫ ТЕЛА (ИМТ): " + a + "\n" +
 
                 "-Норма меньше 25\n" +
                 "-Избыточный вес 25-29,9\n" +
                 "-I степень ожирения 30-34,9\n" +
-                "-II степень ожирения 35-39,9\n"+
+                "-II степень ожирения 35-39,9\n" +
                 "-III степень ожирения более 40");
 
-}}
+    }
+}
